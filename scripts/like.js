20038241fa8776c486f66377748225d3bcf74fe5
ProstoLive <artem009saratov@gmail.com>
2025-12-17 01:38:10 +0400

    сделяль

--- scripts/like.js
+++ scripts/like.js
@@ -39,3 +39,46 @@ function setButtonText(heart, button) {
     );
   }
 }
+
+function changeTheme(theme) {
+  document.documentElement.className = '';
+  document.documentElement.classList.add(`theme-${theme}`);
+  localStorage.setItem('theme', theme);
+}
+
+(function initTheme() {
+  const theme = localStorage.getItem('theme');
+  if (theme) {
+    changeTheme(theme);
+  }
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.documentElement;
+  const themeButtons = document.querySelectorAll('.theme-menu__button');
+
+  function setDisabled(theme) {
+    themeButtons.forEach((item) => {
+      if (item.getAttribute('data-theme') === theme) {
+        item.setAttribute('disabled', true);
+      } else {
+        item.removeAttribute('disabled');
+      }
+    });
+  }
+
+  if ([...root.classList].includes('theme-light')) {
+    setDisabled('light');
+  } else if ([...root.classList].includes('theme-dark')) {
+    setDisabled('dark');
+  } else {
+    setDisabled('auto');
+  }
+
+  themeButtons.forEach((button) => {
+    button.onclick = () => {
+      changeTheme(button.getAttribute('data-theme'));
+      setDisabled(button.getAttribute('data-theme'));
+    };
+  });
+});
